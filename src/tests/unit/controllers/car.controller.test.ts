44ab8d29101d5dc02178ff4fadf0a02672be2a53
GugaSantos <guga.santos.guga@gmail.com>
2022-09-14 18:58:07 -0300

    test: carService and carController create and readOne

---
+++ src/tests/unit/controllers/car.controller.test.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import { Request, Response } from 'express';
+import * as sinon from 'sinon';
+
+import CarsController from '../../../controllers/Cars';
+import Cars from '../../../models/carsModel';
+import CarsService from '../../../services/Cars';
+import { carMock, carMockWithId } from '../../mocks/carMock';
+
+describe('Cars Controller', () => {
+  const carModel = new Cars()
+  const carService = new CarsService(carModel);
+  const carController = new CarsController(carService);
+ 
+  const req = {} as Request; 
+  const res = {} as Response;
+
+  before(() => {
+    sinon.stub(carService, 'create').resolves(carMock);
+    sinon.stub(carService, 'readOne').resolves(carMock);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(() => {
+    sinon.restore()
+  })
+
+  describe('Create a car', () => {
+    it('On success', async () => {
+      req.body = carMock;
+      await carController.create(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    });
+  });
+
+  describe('ReadOne Car', () => {
+    it('On success', async () => {
+      req.params = { id: carMockWithId._id };
+      await carController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    });
+  });
+});
---
+++ src/tests/unit/services/car.service.test.ts
@@ -0,0 +1,60 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
+import Cars from '../../../models/carsModel';
+import CarsService from '../../../services/Cars';
+import { carMock, carMockWithId } from '../../mocks/carMock';
+
+describe('Car Service', () => {
+	const carModel = new Cars();
+	const carService = new CarsService(carModel);
+
+	before(() => {
+		sinon.stub(carModel, 'create').resolves(carMockWithId);
+		sinon.stub(carModel, 'readOne')
+      .onCall(0).resolves(carMockWithId) 
+			.onCall(1).resolves(null); 
+	})
+	after(() => {
+		sinon.restore()
+	})
+	describe('Create Cars', () => {
+		it('On success', async () => {
+			const newCar = await carService.create(carMock);
+
+			expect(newCar).to.be.deep.equal(carMockWithId);
+		});
+
+		it('On failure', async () => {
+      let error;
+			try {
+				await carService.create({});
+			} catch (err) {
+       error = err
+			}
+
+       expect(error).to.be.instanceOf(ZodError);
+		});
+	});
+
+	describe('ReadOne Car', () => {
+		it('On success', async () => {
+			const newCar = await carService.readOne(carMockWithId._id);
+
+			expect(newCar).to.be.deep.equal(carMockWithId);
+		});
+
+		it('On failure', async () => {
+    let error;
+			try {
+				await carService.readOne(carMockWithId._id);
+			} catch (err:any) {
+       error = err
+			}
+
+      expect(error, 'error should be defined').not.to.be.undefined;
+      expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+		});
+	});
+});
