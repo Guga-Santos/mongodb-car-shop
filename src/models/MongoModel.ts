e0acfe348ba0081a1e0c23eb5ddd0e23c06dbc34
GugaSantos <guga.santos.guga@gmail.com>
2022-09-14 15:52:37 -0300

    feat: create MongoModel

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,35 @@
+import { isValidObjectId, Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  contructor(model: Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async read(): Promise<T[] | null> {
+    return this._model.find();
+  }
+
+  public async readOne(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
+
+  public async update(_id: string, obj: Partial<T>): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+    return this._model.findOneAndUpdate({ _id }, obj);
+  }
+
+  public async delete(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+    return this._model.findByIdAndDelete({ _id });
+  }
+}
+
+export default MongoModel;
